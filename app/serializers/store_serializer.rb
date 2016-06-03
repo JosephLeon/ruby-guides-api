a73027a34d88fef1e60ff7b4740a38587259a85e
Joseph Leon <joseph.leon.9@gmail.com>
2016-06-02 20:55:17 -0400

    Clean code using rubocop.

--- app/serializers/store_serializer.rb
+++ app/serializers/store_serializer.rb
@@ -1,3 +1,5 @@
+# Control the output of data to the json array for stores controller.
 class StoreSerializer < ActiveModel::Serializer
-  attributes :id, :name, :address, :address2, :city, :state, :zip, :country, :phone, :website, :uuid
+  attributes :id, :name, :address, :address2, :city,
+             :state, :zip, :country, :phone, :website, :uuid
 end
