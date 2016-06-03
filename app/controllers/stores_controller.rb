9b1d09002c157de49243eafe49ba7d69199d87f8
Joseph Leon <joseph.leon.9@gmail.com>
2016-06-02 20:49:50 -0400

    Use rubocop to clean up stores controller.

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,8 +1,7 @@
+# Class for stores which serves json data as an API.
 class StoresController < ApplicationController
-	
-	def index
-		@stores = Store.all
-		render json: @stores
-	end
-
+  def index
+    @stores = Store.all.paginate(page: params[:page], per_page: 50)
+    render json: { stores: @stores }
+  end
 end
