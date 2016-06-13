7b03b998f62844c217af60c76d7c7f05fc47a2b0
Joseph Leon <joseph.leon.9@gmail.com>
2016-06-12 22:20:46 -0400

    Add stores controller spec tests file, add some tests to check for http status on index and show.

---
+++ spec/controllers/stores_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::StoresController, :type => :controller do
+  describe "GET #index" do
+    it "responds successfully with an HTTP 200 status code" do
+      get :index
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+
+    # it "renders the index template" do
+    #   get :index
+    #   expect(response).to render_template("index")
+    # end
+
+    # it "loads all of the posts into @posts" do
+    #   post1, post2 = Post.create!, Post.create!
+    #   get :index
+
+    #   expect(assigns(:posts)).to match_array([post1, post2])
+    # end
+  end
+
+  describe "GET #show/:id" do
+    it "responds with an HTTP 200 status code" do
+      @store = Store.find(1)
+      get :show, id: @store
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+  end
+end
